7a64ea2e77105e3837acf4b650769dfd0caa993a
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-07-10 20:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var mus= []
+  for (var i = 0; i < musicians.length; i++) {
+    var array= musicians[i]+" "+ "plays"+" "+instruments[i]
+    mus.push(array)
+  }
+return mus
+}
+
+function johnLennonFacts(facts) {
+var mus=[]
+var i=0
+while (i<facts.length) {
+  var array= facts[2] +"!!!"
+  mus.push(array)
+  var b= ++i
+}
+return array
+}
